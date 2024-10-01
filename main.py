4c00146480e1ba0ef687239be6b96d70057cd938
nicholas-tankiang <nicholas.tankiang210@gmail.com>
2024-10-01 20:21:10 +0800

    change display order in while loop

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    #print("Starting asteroids!")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()

0b3078c5914e8e14e7d9e18f96b9ceaa5646a8cf
nicholas-tankiang <nicholas.tankiang210@gmail.com>
2024-10-01 22:26:20 +0800

    add player object

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    #print("Starting asteroids!")
 
     frames = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_object = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         pygame.Surface.fill(screen, (0,0,0))
+        player_object.draw(screen)
         pygame.display.flip()
+
+        # set to 60 fps
         dt = frames.tick(60) / 1000
 
 if __name__ == "__main__":

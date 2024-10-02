aab50688fb83593164542591349363fc3a46a7cd
nicholas-tankiang <nicholas.tankiang210@gmail.com>
2024-10-02 15:43:01 +0800

    add shoot shot function to player, fix shot init

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from bullet import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -33,7 +34,14 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-abs(dt))
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot_object = Shot(self.position.x, self.position.y)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot_object.velocity = forward * PLAYER_SHOOT_SPEED
